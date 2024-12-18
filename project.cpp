de0b7a12a3490c1dffee064ded03a1e058ee0b91
naikalam07 <naikalamakhss0007@gmail.com>
2024-12-19 00:57:27 +0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,3 +1,16 @@
-int main(){
-  return 0;
-}
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Constants
+const int MAX_UNIVERSITIES = 10;  // Maximum number of universities to manage
+
+// Arrays to store university data (names are pretty self explanatory)
+string universityNames[MAX_UNIVERSITIES]; 
+string locations[MAX_UNIVERSITIES];
+int tuitionFees[MAX_UNIVERSITIES];
+string majors[MAX_UNIVERSITIES];
+bool admissionStatus[MAX_UNIVERSITIES];
+int minimumPercentages[MAX_UNIVERSITIES];

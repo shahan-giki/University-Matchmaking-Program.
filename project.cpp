278899444ad8354432cfa22f58969c117c07b028
naikalam07 <naikalamakhss0007@gmail.com>
2024-12-19 01:05:49 +0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -14,3 +14,14 @@ int tuitionFees[MAX_UNIVERSITIES];
 string majors[MAX_UNIVERSITIES];
 bool admissionStatus[MAX_UNIVERSITIES];
 int minimumPercentages[MAX_UNIVERSITIES];
+
+
+// Function Prototypes
+void loadUniversityData();
+void showMainMenu();
+void displayMenu();
+void showUniversityDetails(int index);
+void getStudentInput();
+void displayMatchingUniversities(string preferredField, int minPercentage, int maxTuition, string preferredLocation); 
+void getValidChoice(int& choice);
+
